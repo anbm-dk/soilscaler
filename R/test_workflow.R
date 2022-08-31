f4a40854c14163bbc04632ca1490796365922fea
Anders Bjørn Møller <perserkongen@gmail.com>
2022-08-31 13:28:17 +0200

    Just a demo
    For demonstration

--- R/test_workflow.R
+++ R/test_workflow.R
@@ -94,7 +94,239 @@ for(i in 1:length(sites))
 
 # Load soil observations
 
+obs <- sites %>%
+  sapply(
+    function(x)
+    {
+      x %>%
+        paste0(root, '/data/soil_observations/'
+               , ., '_coords_tex.txt'
+               ) %>%
+        read.table(header = TRUE
+                   , sep = '\t'
+                   )
+    }
 
+  )
+
+mincols <- lapply(obs, function(x) ncol(x)) %>%
+  unlist %>%
+  min
+
+obs %<>% lapply(function(x)
+  select(x, 1:mincols)
+  )
+
+coord_cols <- obs[[1]] %>%
+  colnames %>%
+  .[2:3]
+
+crs_all <- crs(field_list[[1]])
+
+obs %<>% lapply(function(x)
+  vect(x
+       , geom = coord_cols
+       , crs = crs_all
+       , keepgeom = TRUE
+       )
+)
+
+# Assign the correct crs
+
+for(i in 1:length(sites))
+{
+  crs(cov_list[[i]])   <- crs_all
+  crs(EC_list[[i]])    <- crs_all
+  crs(ortho_list[[i]]) <- crs_all
+}
+
+# Assign names to lists
+
+names(cov_list) <- sites
+names(EC_list) <- sites
+EC_list <- lapply(
+  EC_list
+  , function(x)
+  {
+
+    names(x) <- 'EC_00_25_cm'
+    return(x)
+  }
+)
+names(field_bare_list) <- sites
+names(field_list) <- sites
+ortho_list <- lapply(
+  ortho_list
+  , function(x)
+  {
+
+    x <- subset(x
+                , c(3,2,1))
+    names(x) <- c('red', 'green', 'blue')
+    return(x)
+  }
+)
+names(ortho_list) <- sites
+
+# Load input maps
+
+DK_2014 <- root %>%
+  paste0(., '/data/input_maps/DK_2014_lite/') %>%
+  list.files(full.names = TRUE) %>%
+  rast
+
+soilgrids <- root %>%
+  paste0(., '/data/input_maps/soilgrids/') %>%
+  list.files(full.names = TRUE) %>%
+  rast
+
+divisors <- c(rep(10, 6), rep(100, 2))
+
+soilgrids <- ifel(soilgrids == 32767.00, NA, soilgrids/divisors)
+
+# Run a model
+# Define parameters
+
+observations <- obs
+target <- 'clay'
+
+useDK_map <- FALSE
+if(useDK_map)
+{
+  coarse <- DK_2014$DK_clay_mean
+  coarse_unc <- DK_2014$DK_clay_SD
+} else {
+  coarse <- soilgrids$DK_00_20_clay_mean
+  coarse_unc <- soilgrids$DK_00_20_clay_unc
+}
+
+fine <- EC_list
+
+flatten_coarse <- FALSE
+
+# Add checks for consistency
+
+# Extract coarse
+
+if(!is.null(coarse_unc))
+{
+  coarse <- c(coarse, coarse_unc)
+  names(coarse) <- c('coarse', 'coarse_unc')
+}
+
+coarse_resampled <- list()
+
+if(!flatten_coarse)
+{
+  for(i in 1:length(fine))
+  {
+    if(terra::crs(coarse, proj = TRUE) ==
+       terra::crs(fine[[i]], proj = TRUE))
+    {
+      coarse_resampled[[i]] <- terra::resample(coarse, fine[[i]])
+    } else {
+      coarse_resampled[[i]] <- terra::project(coarse, fine[[i]])
+    }
+    coarse_resampled[[i]] %<>% terra::mask(., mask = fine[[i]])
+    observations[[i]] %<>%
+      terra::extract(coarse_resampled[[i]], .) %>%
+      dplyr::select(., -1) %>%
+      merge(observations[[i]], .)
+  }
+} else {
+  for(i in 1:length(observations))
+  {
+    if(terra::crs(coarse, proj = TRUE) !=
+       terra::crs(observations[[i]], proj = TRUE))
+    {
+      coarse_i <- observations[[i]] %>%
+        terra::project(., y = coarse) %>%
+        terra::extract(coarse, .) %>%
+        dplyr::select(., -1)
+
+    } else {
+      coarse_i <- observations[[i]] %>%
+        terra::extract(coarse, .) %>%
+        dplyr::select(., -1)
+    }
+    mean_i <- coarse_i %>%
+      apply(., 2, mean)
+    observations[[i]] %<>% merge(., coarse_i)
+    coarse_resampled[[i]] <- fine[[i]]*0 + mean_i
+  }
+}
+
+
+# Plot results
+
+library(ggplot2)
+library(gridExtra)
+library(rasterVis)
+
+plotted <- coarse_resampled
+lyr <- 1
+
+mybreaks <- function(x, interval = 100)
+{
+  out <- seq(from = x[1], to = x[2], by = interval)
+  return(out)
+}
+
+if(length(names(plotted[[1]])) > 1)
+{
+  plotlist <- lapply(
+    plotted
+    , function(x)
+    {
+      out <- gplot(x[[lyr]]) +
+        geom_tile(aes(fill = value)) +
+        facet_wrap(~ variable) +
+        coord_equal() +
+        scale_x_continuous(breaks = mybreaks) +
+        scale_y_continuous(breaks = mybreaks) +
+        scale_fill_continuous(na.value = NA) +
+        theme(axis.text.x=element_blank(),
+              axis.text.y=element_blank(),
+              axis.ticks=element_blank(),
+              axis.title.x=element_blank(),
+              axis.title.y=element_blank())
+      return(out)
+    }
+  )
+} else {
+  plotlist <- lapply(
+    plotted
+    , function(x)
+    {
+      out <- gplot(x) +
+        geom_tile(aes(fill = value)) +
+        coord_equal() +
+        scale_x_continuous(breaks = mybreaks) +
+        scale_y_continuous(breaks = mybreaks) +
+        scale_fill_continuous(na.value = NA) +
+        theme(axis.text.x=element_blank(),
+              axis.text.y=element_blank(),
+              axis.ticks=element_blank(),
+              axis.title.x=element_blank(),
+              axis.title.y=element_blank())
+      return(out)
+    }
+  )
+}
+
+do.call("grid.arrange"
+        , c(plotlist
+            , ncol = 3
+            )
+        )
+
+
+# To do
+# 1: Crop covariates to field extent
+# 2: Round off decimals (covariates, EC, ortho)
+# 3: Import input maps (OK)
+# 4: Crop input maps to areas surrounding fields
+# 5: Give names to all the lists (OK)
 
 # # Playing around with colors
 #
